b03bb381cf143f03b7d30ee6498a0995ffedba46
Derek Passen <dpassen1@gmail.com>
2022-12-07 10:49:25 -0600

    small cleanups

--- 2022/day-7/src/day_7.clj
+++ 2022/day-7/src/day_7.clj
@@ -40,23 +40,29 @@
 (defn- directory-size
   [tree ks]
   (->> (get tree ks)
-       (mapcat (fn [[file size]]
-                 (if (neg? size)
-                   (directory-size tree (conj ks (keyword file)))
-                   [size])))))
+       (mapcat
+        (fn [[file size]]
+          (if (neg? size)
+            (directory-size tree (conj ks (keyword file)))
+            [size])))))
 
 (defn part-one
   []
   (with-open [rdr (io/reader (io/resource "./input.txt"))]
     (let [input (line-seq rdr)
           world (reduce construct-world {} input)]
-      (->> (:tree world)
-           (map (fn [[ks _dir]]
-                  [ks (reduce + 0 (directory-size (:tree world) ks))]))
-           (filter (fn [[_ks size]]
-                     (>= 100000 size)))
-           (map second)
-           (reduce + 0)))))
+      (transduce
+       (comp
+        (map
+         (fn [[ks _dir]]
+           [ks (reduce + 0 (directory-size (:tree world) ks))]))
+        (filter
+         (fn [[_ks size]]
+           (>= 100000 size)))
+        (map second))
+       +
+       0
+       (:tree world)))))
 
 (def needed   30000000)
 (def max-size 70000000)
@@ -64,15 +70,20 @@
 (defn part-two
   []
   (with-open [rdr (io/reader (io/resource "./input.txt"))]
-    (let [input       (line-seq rdr)
-          world       (reduce construct-world {} input)
-          root-size   (reduce + 0 (directory-size (:tree world) [:root]))
-          unused-size (- max-size root-size)
+    (let [input         (line-seq rdr)
+          world         (reduce construct-world {} input)
+          root-size     (reduce + 0 (directory-size (:tree world) [:root]))
+          unused-size   (- max-size root-size)
           required-size (- needed unused-size)]
       (->> (:tree world)
-           (map (fn [[ks _dir]]
-                  [ks (reduce + 0 (directory-size (:tree world) ks))]))
-           (filter (fn [[_ks size]]
-                     (<= required-size size)))
+           (map
+            (fn [[ks _dir]]
+              [ks (reduce + 0 (directory-size (:tree world) ks))]))
+           (filter
+            (fn [[_ks size]]
+              (<= required-size size)))
            (apply min-key second)
            second))))
+
+[(part-one)
+ (part-two)]
